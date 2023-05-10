f3a62a8caba3eae0c9e14fd2faf87d9a4a144078
VulpeComunicacao <contato@vulpecomunicacao.com.br>
2023-05-10 10:55:00 -0300

    #fix - espacamento nos valores

--- ansegtv/index.php
+++ ansegtv/index.php
@@ -123,9 +123,13 @@
                         <li>
                             <h3>Valores</h3>
                             <p><b>Livre concorrência:</p> Defender a livre concorrência, a abertura de mercado, o relacionamento ético entre as empresas e a observância dos princípios legais;</p>
+                            <br>
                             <p><b>Inovação:</p> Incentivar a pesquisa e a inovação, bem como a parceria na cadeia produtiva;</p>
+                            <br>
                             <p><b>Fortalecimento da segurança:</p> Apoiar e aprimorar as melhores práticas relativas à segurança das operações;</p>
+                            <br>
                             <p><b>Diálogo com a sociedade:</p> Defender o diálogo, o respeito e a transparência nas relações com os clientes, com o Estado e com a sociedade;</p>
+                            <br>
                             <p><b>Parceria com órgão regulador e demais instituições de segurança:</p> Atuar em parceria com as autoridades na troca de informações e fornecimento de subsídios que contribuam com o fortalecimento da segurança pública e privada.</p>
                         </li>
                     </ul>
