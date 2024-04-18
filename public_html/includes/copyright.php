114dce4a940cded6127b8a331b88d8756e918b37
Vulpe <contato@vulpecomunicacao.com.br>
2024-04-18 16:43:14 -0300

    Remove: remoção de arquivos desnecessários

--- public_html/includes/copyright.php
+++
@@ -1,9 +0,0 @@
-<section id="copyright">
-  <div class="container-fluid">
-    <div class="row">
-        <div class="col">
-        <p>© Copyright - 2022 - Todos os Direitos Reservados. Synapse Brasil <!-- <button class="ativa-aviso-lgpd" onclick="window.sendAdoptCommand('open')">Mostrar Aviso LGPD </button> --> | <a href="https://ansegtv.com.br/politica-de-privacidade" title="Política de Privacidade">Política de Privacidade</a></p>
-        </div>
-    </div>
-  </div>
-</section>
