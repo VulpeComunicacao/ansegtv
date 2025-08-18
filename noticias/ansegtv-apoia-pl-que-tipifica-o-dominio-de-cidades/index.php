adc6162ed8f934af32f9aa5656c4e2df78d72217
brunorsilvadf <contato@vulpecomunicacao.com.br>
2025-08-18 18:43:42 -0300

    atualização - endereço Footer - TEste

--- noticias/ansegtv-apoia-pl-que-tipifica-o-dominio-de-cidades/index.php
+++ noticias/ansegtv-apoia-pl-que-tipifica-o-dominio-de-cidades/index.php
@@ -137,7 +137,7 @@
 <section id="footer" class="container">
             <div class="row">
                 <div class="col-12 mb-3 contato-institucional text-center">
-                    <p><strong>Endereço:</strong> Q 01 SAUS, Ed. Terra Brasilis, Salas 1102 e 1103 | CEP 70070-941 Brasília - DF</p>
+                    <p><strong>Endereço:</strong> SAUS, Quadra 1, Bloco M, Edifício Libertas, Sala 303 | 70.070-935 | Brasília - DF</p>
                     <p><strong>E-mail:</strong> <a href="mailto:diretoria@ansegtv.com.br">diretoria@ansegtv.com.br</a></p>                
                     
                 </div>
