0814f0739748ca836054627d9e3a235c2dcd6098
brunorsilvadf <contato@vulpecomunicacao.com.br>
2026-02-13 15:40:04 -0300

    fix: troca da imagem tb forte Cadastro de nova matéria

---
+++ noticias/joiandro-carvalho-assume-a-presidencia-da-ansegtv/index.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="pt-BR">
+        <!-- Head Section -->
+        <head>
+            <title>Joiandro Carvalho assume a presidência da ANSEGTV 
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="robots" content="index, follow">
+            <meta name="google-site-verification" content="fr91pley1eSdpCFMb-YbDS8l9GqIjBnP8ZsXShUXsRg" />
+            <!-- OpenGraph TAGS -->
+            <meta property="og:type" content="article">
+            <meta name="author" content="ANSEGTV">
+            <meta property="og:title" content="Joiandro Carvalho assume a presidência da ANSEGTV 
+">
+            <meta meta name="description" property="og:description" content="O novo presidente da ANSEGTV (Associação Nacional de Segurança e Transporte de Valores), Joiandro Carvalho, tomou posse no dia 26 de novembro de 2025">
+            <meta property="og:site_name" content="ANSEGTV">
+            <meta property="og:url" content="https://ansegtv.com.br/noticias/novo-presidente-da-ansegtv-toma-posse-em-brasilia/">
+            <meta property="og:image" content="https://ansegtv.com.br/img/uploads/2026/02/joiandro-carvalho-share.jpg">
+            <meta name="image" property="og:image" content="https://ansegtv.com.br/img/uploads/2026/02/joiandro-carvalho-share.jpg">
+            <meta property="article:published_time" content="2026-02-13"/>
+
+            <!-- Bootstrap CSS -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+            <!-- Custom CSS -->
+            <link rel="stylesheet" href="../../css/style.css">
+
+	    <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RP1BVPHJHD"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-RP1BVPHJHD');
+            </script>
+
+  </head>
+
+  <body>
+
+        <!-- Navigation Section -->
+        <nav class="navbar navbar-expand-lg static-top navbar-light px-3">
+            <div class="container">
+                <a class="navbar-brand" href="../../">
+                <img src="../../img/logo-header.png" height="140px" alt="Logo ANSEGTV">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link" href="../../">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="../../ansegtv/">ANSEGTV</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="../../estrutura/">Estrutura</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="../../indice-inflacao/">Índice de Inflação</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="../../noticias/">Notícias</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="../../contato/">Contato</a>
+                    </li>
+                </ul>
+                <ul class="navbar-social">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.linkedin.com/company/ansegtv/" target="_blank"><i class="fa fa-linkedin"></i> </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.instagram.com/ansegtvoficial/" target="_blank"><i class="fa fa-instagram"></i> </a>
+                    </li>
+                </ul>
+                
+                </div>
+            </div>
+        </nav>
+
+        <!-- News Body -->
+            <section id="news-body" class="mt-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-10 offset-md-1 title-news text-center">
+                            <h1>Joiandro Carvalho assume a presidência da ANSEGTV </h1>
+                            <p class="post-infos">
+                                <span class="mx-2 author"><i class="fa fa-user"></i> ANSEGTV</span> | <span class="mx-2 date"><i class="fa fa-clock-o"></i>13/02/2026 - 15h42</span> | <span class="mx-2 category"><i class="fa fa-tag"></i> ANSEGTV</span>
+                            </p>
+                            <hr class="divider">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 col-md-10 offset-md-1 content-news">
+
+                        <p class="article">O novo presidente da ANSEGTV (Associação Nacional de Segurança e Transporte de Valores), Joiandro Carvalho, tomou posse no dia 26 de novembro de 2025, após uma Assembleia Geral Extraordinária que contou com a presença de toda a diretoria da associação.
+                        </p>
+
+                        <figure class="pull-right m-2 text-left">
+                                <img src="../../img/uploads/2026/02/joiandro-ansegtv.png" width="450" height="auto" alt="Joiandro Carvalho, novo presidente da ANSEGTV" class=" img-fluid">	
+                                <figcaption</figcaption>
+                        </figure>
+
+                        <p class="article">Atualmente Superintendente de Segurança e Infraestrutura da TBForte – uma das empresas associadas à ANSEGTV – Joiandro Carvalho assume a presidência reafirmando o compromisso de atuar em defesa dos interesses das empresas que prestam serviços de segurança, transporte de valores, gestão de ciclo numerário, custódia, logística de valores, entre outras atividades do setor. Ele aproveitou o momento para agradecer e parabenizar ao Vanderlei Reis pela condução da presidência da ANSEGTV nos últimos dezoito meses.
+                        </p>
+                        
+                        <p class="article">E na mesma ocasião, a ANSEGTV também reforçou os seus valores institucionais, destacando a defesa da livre concorrência, da abertura de mercado, da ética nas relações empresariais e do cumprimento rigoroso dos princípios legais.</p>
+
+                        </div>
+                    </div>
+                <br>
+                <br>
+                <div class="col text-center mt-4"> <a href="../" class="voltar">Veja todas as notícias</a></div>
+                </div>
+            </div>
+        </section>
+    <!-- Footer Section -->
+    <?php
+    // Incluir sistema centralizado de informações de contato
+    require_once '../../includes/contact-info.php';
+    
+    // Renderizar footer de contato
+    render_contact_footer();
+    ?>
+    
+        <?php include_once '../../includes/copyright.php'; ?>
+    
+    <!-- Boostrap and Custom JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+
+        $('.carousel').carousel({
+        interval: 5000
+        })
+
+        $('ul.nav li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300);
+        }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(300);
+        });
+        
+    </script>
+  <script src="//tag.goadopt.io/injector.js?website_code=98bfaecd-beda-4252-b173-f964b7a9a092" class="adopt-injector"></script>
+ </body>
+</html>
